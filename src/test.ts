a8439112299e9fa6040fff3f8c6fdb1315ed5e99
ananya <ananya.sahu0894@gmail.com>
2023-11-30 09:39:41 +0000

    adding tests for verfying page content

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,11 @@
 
 import {Browser, chromium, Page} from "playwright";
+// import {SelectorConstants} from "./SelectorConstants";
 import {firefox} from "playwright/index";
 
-
-describe('Ananya Portfolio', () => {
+describe('VehicleSearchComponent', () => {
 let browser: Browser;
     let page: Page;
-
     beforeAll(async () => {
 
         browser = await firefox.launch({
@@ -25,4 +24,58 @@ let browser: Browser;
         })
     });
 
-});
+    afterAll(async () =>{
+        await browser.close();
+    });
+
+    it("Open page", async () => {
+            await page.goto("http://ananyasahu.com/?i=1");
+            await page.waitForTimeout(10000);
+            // page load
+            // await page.waitForResponse(response => {
+            //     return response.request().resourceType() === "xhr";
+            // });
+        }, 100000);
+
+    it("Open page", async () => {
+        let xxx : string|null ;
+             await page.waitForSelector('//*[@id="home"]/h2');
+             page.textContent('//*[@id="home"]/h2').then(
+                res => {
+                    xxx = res != null ? res.toString():  null
+                    if(xxx == 'HELLO, I AM ANANYA SAHU!') {
+                        return true
+                        // print('true')
+                    } else {
+                        return false
+                        // print('true')
+                    }
+
+                });
+            
+           
+            //  await page.click(SelectorConstants.IDP_USERNAME_INPUT)
+            //  await page.fill(SelectorConstants.IDP_USERNAME_INPUT, username)
+         
+        }, 100000); 
+
+        it("click hire me", async () => {
+            let xxx : string|null ;
+                 await page.waitForSelector('//*[@id="home"]/a');               
+                await page.click('//*[@id="home"]/a')
+                // print('clicked')
+                //  await page.fill(SelectorConstants.IDP_USERNAME_INPUT, username)
+             
+            }, 100000); 
+
+            it("click services link and navigate to services", async () => {
+                // let xxx : string|null ;
+                    await page.waitForSelector('//*[@id="collapsible-nav"]/a[2]');               
+                    await page.click('//*[@id="collapsible-nav"]/a[2]')
+                    // page.
+                    // print('clicked')
+                    //  await page.fill(SelectorConstants.IDP_USERNAME_INPUT, username)
+                 
+                }, 100000); 
+            
+    });
